0f5c0ec048a7402a6eb7a57a291caeb35209f323
Mano <apple@Macintosh.local>
2021-02-01 19:49:51 +0300

    JS example

---
+++ story.js
@@ -0,0 +1,7 @@
+let name = "Abdulrahman";
+let age = 35;
+let hobby = "Tennis";
+
+console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${age + 33} years old. By then you\'ll have decades of ${hobby} practice!`);
+
+
